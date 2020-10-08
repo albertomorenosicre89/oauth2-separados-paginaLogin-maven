bb7045ffd47b8c97aa9a42a83449f5effaa88fc0
albertomorenosicre89 <albertomorenosicre89@gmail.com>
2020-10-08 15:14:44 +0200

    Refactor

--- autentication-login/src/main/java/com/santander/oauth2/autenticationlogin/UserController.java
+++ autentication-login/src/main/java/com/santander/oauth2/autenticationlogin/UserController.java
@@ -8,7 +8,7 @@ import java.security.Principal;
 @RestController
 public class UserController {
 
-    @GetMapping("/user/me")
+    @GetMapping("/user")
     public Principal user(Principal principal) {
         return principal;
     }
